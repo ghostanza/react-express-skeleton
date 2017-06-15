eb33262081dc9ecc044c74c51ec4ab472c749257
ghostanza <friedcaligari@gmail.com>
2017-06-14 22:13:50 -0400

    update LoginButton for heroku

--- src/app/components/pages/page-components/dashboard/LoginButton.js
+++ src/app/components/pages/page-components/dashboard/LoginButton.js
@@ -4,7 +4,7 @@ export default class LoginButton extends React.Component {
   render() {
     var auth_link = 'http://accounts.spotify.com/authorize',
         client_id = 'c3f3e6c8e0054a499d67a714c264e10e',
-        redirect_uri = encodeURIComponent('http://localhost:8080/api/v1/callback'),
+        redirect_uri = document.domain === 'sheltered-mountain-31019.herokuapp.com' ? encodeURIComponent('https://sheltered-mountain-31019.herokuapp.com/api/v1/callback') : encodeURIComponent('http://localhost:8080/api/v1/callback'),
         scope = encodeURIComponent('user-read-email user-top-read playlist-modify-public user-read-currently-playing user-read-recently-played user-read-playback-state'),
         response_type = 'code',
         login_link = `${auth_link}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=${response_type}`;
