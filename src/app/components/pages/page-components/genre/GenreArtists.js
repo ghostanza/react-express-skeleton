d2289386883bab5f347dc58f2a15c08eff7ef55f
ghostanza <friedcaligari@gmail.com>
2017-06-19 23:02:57 -0400

    updated genre artist page to use cards, removed header and welcome

--- src/app/components/pages/page-components/genre/GenreArtists.js
+++ src/app/components/pages/page-components/genre/GenreArtists.js
@@ -8,10 +8,9 @@ class GenreArtists extends React.Component {
         <ul>
           { this.props.artists.map((artist) => {
             return (
-              <li key={artist.id}>
+              <li key={artist.id} style={{'backgroundImage' : artist.images[1] ? `url(${artist.images[1].url})` : `url(${artist.images[0].url})` }}>
                 <Link to={`/artist/${artist.id}`}>
                   <span className='genre-hover'>{artist.name}</span>
-                  <img src={artist.images[1] ? artist.images[1].url : artist.images[0].url}/>
                 </Link>
               </li>
             )
