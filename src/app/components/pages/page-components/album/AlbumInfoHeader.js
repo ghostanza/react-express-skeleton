e4162c1a001d0df75fe69928eeaa669d0d35893a
ghostanza <friedcaligari@gmail.com>
2017-07-05 19:58:32 -0400

    added label to album page to view more artists in that label

--- src/app/components/pages/page-components/album/AlbumInfoHeader.js
+++ src/app/components/pages/page-components/album/AlbumInfoHeader.js
@@ -8,8 +8,9 @@ export default class AlbumInfoHeader extends React.Component {
         {this.props.info.images && this.props.info.images.length > 1 ? (
           <img src={this.props.info.images[1].url}/>
         ) : ''}
-        <h3 className="album-artist">{this.props.info.artists && this.props.info.artists.length ? (<Link to={`/artist/${this.props.info.artists[0].id}`}>{this.props.info.artists[0].name}</Link>) : ''}</h3>
-        <h2 className="album-name">{this.props.info.name ? this.props.info.name : ''}</h2>
+        {this.props.info.artists && this.props.info.artists.length ? (<h3 className="album-artist"><Link to={`/artist/${this.props.info.artists[0].id}`}>{this.props.info.artists[0].name}</Link></h3>) : ''}
+        {this.props.info.label ? (<h3 className="album-label"><Link to={`/search/label/${this.props.info.label}`}>{this.props.info.label}</Link></h3>):''}
+        {this.props.info.name ? (<h2 className="album-name">{this.props.info.name}</h2>) : ''}
       </div>
     )
   }
